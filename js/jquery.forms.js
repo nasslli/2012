977a88f14db7382e131dc9627fe7f0e263051b79
Christopher Brown <io@henrian.com>
2015-06-14 14:55:30 -0500

    Add compilation of current NASSLLI 2012 site

---
+++ js/jquery.forms.js
@@ -0,0 +1,38 @@
+function parseLooseForm($form) {
+  var store = {};
+  $form.children('div').each(function(i, div) {
+    var $div = $(div),
+        value = [],
+        force_list = false;
+  
+    $div.find("input[type='text']").each(function(i, el) {
+      value.push(el.value);
+    });
+    $div.find("textarea").each(function(i, el) {
+      value.push(el.value);
+    });
+    $div.find("input[type='password']").each(function(i, el) {
+      value.push(el.value);
+    });
+    $div.find("select option:selected").each(function(i, el) {
+      value.push($(el).text());
+    });
+    if ($div.find("input[type='checkbox']").length > 1) {
+      force_list = true;
+    }
+
+    // for each checkbox/radiobutton get the id, find the label[for=<that-id>].innerText, use that as value
+    $div.find("input[type='checkbox']:checked").each(function(i, el) {
+      value.push($div.find("label[for='" + el.id + "']").text());
+    });
+    $div.find("input[type='radio']:checked").each(function(i, el) {
+      value.push($div.find("label[for='" + el.id + "']").text());
+    });
+
+    if (!force_list && value.length < 2) {
+      value = value[0];
+    }
+    store[div.id] = value;
+  });
+  return store;
+}
